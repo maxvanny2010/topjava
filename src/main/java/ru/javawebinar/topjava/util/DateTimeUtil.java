37a4120c435dff40bed922840d921907d988c09f
Maxim <mailtime2010@gmail.com>
2020-06-16 07:32:24 -0400

    HW02_isBetween

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -21,6 +21,12 @@ public final class DateTimeUtil {
         return lt.compareTo(startDay) >= 0 && lt.compareTo(endDay) < 0;
     }
 
+    // выглядит ровно, но падает в MealsUtil первый параметр, тк он член класса а не Т.
+    // и как следсвие падает в контроллере. как сделать на данный момент не знаю.
+    public static <T extends LocalDateTime> boolean isBetweenHalfOpen(T lt, T startTime, T endTime) {
+        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
+    }
+
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
     }
